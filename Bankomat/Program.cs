e40c0450b6c756aa0897df5607ee990c9aac78a8
Arman KZ <armansauyenov@gmail.com>
2018-04-28 00:42:50 +0600

    Add project files.

---
+++ Bankomat/Program.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using GeneratorName;
+using KKB.Bank.Module;
+
+namespace Bankomat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string login = "";
+            string password = "";
+
+            try
+            {
+                Client client = new Client();
+                Service.createClient(ref client);
+                client.Login = "admin";
+                client.Password = "admin";
+
+                while (!client.isBlocked)
+                {
+                    #region
+
+                    Console.Clear();
+                    Console.Write("введите логин:");
+                    login = Console.ReadLine();
+                    Console.Write("введите пароль:");
+                    password = Console.ReadLine();
+
+                    if (login != client.Login && password != client.Password)
+                    {
+                        client.WrongField--;
+                        Console.WriteLine("Некорректный введены данные. Осталось {0} попыток", client.WrongField);
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    #endregion
+                }
+
+                if (login == client.Login && password == client.Password)
+                {
+                    #region
+                    if (client.isBlocked)
+                    {
+                        Console.WriteLine("Пользователь заблокирован!");
+                    }
+                    else
+                    {
+                        #region 
+                        do
+                        {
+                            Console.Clear();
+
+                            Console.WriteLine("1) Список счетов");
+                            Console.WriteLine("2) Создать счет");
+                            Console.WriteLine("3) Пополнить счёт");
+                            Console.WriteLine("4) Снять деньги со счёта");
+
+                            Console.WriteLine("6) Выход");
+
+
+                            int menu = 0;
+                            Int32.TryParse(Console.ReadLine(), out menu);
+                            if (menu > 6 || menu < 1)
+                            {
+                                throw new Exception("invalid choice");
+                            }
+                            else
+                            {
+                                switch (menu)
+                                {
+                                    //Список счетов
+                                    case 1:
+                                        {
+                                            client.PrintAccountInfo();
+                                            Console.WriteLine("\n---------------------------------------------\n");
+                                            int choice = 0;
+                                            Console.WriteLine("Введите любое число для возврата в меню, либо 0 для выхода");
+                                            Int32.TryParse(Console.ReadLine(), out choice);
+                                            if (choice == 0)
+                                                return;
+                                            else
+                                                break;
+                                        }
+                                        break;
+                                    //Создать счет
+                                    case 2:
+                                        {
+                                            try
+                                            {
+                                                Account acc = Service.createAccount();
+                                                client.ListAccount.Add(acc);
+                                                Console.WriteLine("\nСчёт успешно добавлен!");
+                                            }
+                                            catch (Exception ex)
+                                            {
+                                                Console.WriteLine(ex.Message);
+                                            }
+                                            Console.WriteLine("\n---------------------------------------------\n");
+                                            int choice = 0;
+                                            Console.WriteLine("Введите любое число для возврата в меню, либо 0 для выхода");
+                                            Int32.TryParse(Console.ReadLine(), out choice);
+                                            if (choice == 0)
+                                                return;
+                                            else
+                                                break;
+                                        }
+                                        break;
+                                    case 3:
+                                        {
+                                            Console.WriteLine("Введите номер счёта: ");
+                                            string accountNumber = Console.ReadLine();
+                                            Console.WriteLine("Введите сумму ");
+                                            string acconutSum = Console.ReadLine();
+                                            try
+                                            {
+                                                foreach (var item in client.ListAccount)
+                                                {
+                                                    if (item.AccountNumber == accountNumber)
+                                                    {
+                                                        item.Balance += double.Parse(acconutSum);
+                                                        Console.WriteLine("\nСчёт {0} успешно пополнен на сумму {1}", accountNumber, acconutSum);
+                                                    }
+                                                }
+                                            }
+                                            catch (Exception ex)
+                                            {
+                                                Console.WriteLine(ex.Message);
+                                            }
+                                            Console.WriteLine("\n---------------------------------------------\n");
+                                            int choice = 0;
+                                            Console.WriteLine("Введите любое число для возврата в меню, либо 0 для выхода");
+                                            Int32.TryParse(Console.ReadLine(), out choice);
+                                            if (choice == 0)
+                                                return;
+                                            else
+                                                break;
+                                        }
+                                        break;
+                                    case 4:
+                                        {
+                                            Console.WriteLine("Введите номер счёта: ");
+                                            string accountNumber = Console.ReadLine();
+                                            Console.WriteLine("Введите сумму ");
+                                            string acconutSum = Console.ReadLine();
+                                            try
+                                            {
+                                                foreach (var item in client.ListAccount)
+                                                {
+                                                    if (item.AccountNumber == accountNumber)
+                                                    {
+                                                        if (double.Parse(acconutSum) > item.Balance)
+                                                        {
+                                                            Console.WriteLine("\nВведённая сумма превышает сумму счёта пользователя");
+                                                        }
+                                                        else
+                                                        {
+                                                            item.Balance -= double.Parse(acconutSum);
+                                                            Console.WriteLine("\nСумма {0} успешно снята со счёта {1}", acconutSum, accountNumber);
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                            catch (Exception ex)
+                                            {
+                                                Console.WriteLine(ex.Message);
+                                            }
+                                            Console.WriteLine("\n---------------------------------------------\n");
+                                            int choice = 0;
+                                            Console.WriteLine("Введите любое число для возврата в меню, либо 0 для выхода");
+                                            Int32.TryParse(Console.ReadLine(), out choice);
+                                            if (choice == 0)
+                                                return;
+                                            else
+                                                break;
+                                        }
+                                        break;
+                                    case 6:
+                                        return;
+                                }
+
+                            }
+                        } while (true);
+
+                        #endregion
+                    }
+                    #endregion
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("User blocked");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            // List<Client> ListClient = new List<Client>();
+
+            //GeneratorName.Generator g= new Generator();
+
+            //Client c1 = new Client();
+
+            //c1.DoB = DateTime.Now.AddYears(-60);
+            //c1.FullName = g.GenerateDefault(Gender.man);
+            //c1.IIN = "970131301448";
+            //c1.Login = "Qwe";
+            //c1.Password = "123";
+            //c1.PhoneNumber = "87475458546";
+
+            //ListClient.Add(c1);
+
+            //c1.ClientInfoPrint();
+        }
+    }
+}
---
+++ KKB.Bank/Module/Client.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KKB.Bank.Module
+{
+    public class Client
+    {
+        public Client()
+        {
+            ListAccount = new List<Account>();
+
+        }
+        private string FullName_;
+        public string FullName
+        {
+            get
+            {
+                return FullName_;
+            }
+            set
+            {
+                /*?<center><b><font size=7>Игнат
+ Голованов
+</font></b></center>*/
+                FullName_ = value
+                    .Replace("<center><b><font size=7>", "")
+                    .Replace("</font></b></center>", "")
+                    ;
+
+            }
+        }
+        private string IIN_;
+        public string IIN
+        {
+            get
+            {
+                return IIN_;
+            }
+            set
+            {
+                if (value.Length == 12)
+                {
+                    IIN_ = value;
+                }
+                else
+                {
+                    throw new Exception("Некорректно введён ИИН");
+                }
+            }
+        }
+        public DateTime DoB { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public List<Account> ListAccount;
+
+        public bool isBlocked { get; set; } = false;
+
+        private int WrongField_ = 3;
+        public int WrongField
+        {
+            get
+            {
+                return WrongField_;
+            }
+            set
+            {
+                if (value == 0)
+                    isBlocked = true;
+                WrongField_ = value;
+            }
+        }
+
+        public void ClientInfoPrint()
+        {
+            Console.WriteLine("{0}\n {1}\n {2}\n {3}\n {4}\n {5}\n", FullName, IIN, Login, Password, PhoneNumber, DoB);
+        }
+        public void PrintAccountInfo()
+        {
+            double sumBalance = 0;
+            foreach (Account item in ListAccount)
+            {
+                Console.WriteLine("KZ: {0}", item.AccountNumber);
+                Console.WriteLine("Date Created: {0} ", item.CreateDate);
+                Console.WriteLine("Exp. Date: {0}", item.CloseDate);
+                Console.WriteLine("Balance: {0}", item.Balance);
+
+                sumBalance += item.Balance;
+            }
+
+            Console.WriteLine("итого сумма на счетах: {0:n0}", sumBalance);
+        }
+        //public void 
+    }
+}
